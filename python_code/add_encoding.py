b203c51c9e9d433eea3f5d899bd4dcebb172d8ad
xubaoxiang <xubaoxiang@fantac.com.cn>
2023-03-23 09:42:56 +0800

    fix add encoding error: only .py files to add

--- python_code/add_encoding.py
+++ python_code/add_encoding.py
@@ -21,14 +21,17 @@ def add_encoding(dir_path):
         if len(sub_files) > 0:
             for sub_file in sub_files:
                 # only python file (.py) to deal with
-                if not sub_file.endswith('.py'):
+                if sub_file.endswith('.py'):
                     sub_file_path = os.path.join(root_path, sub_file)
+
                     # read content
                     with open(sub_file_path, 'r', encoding='UTF-8') as file:
                         file_content = file.read()
+
                     # skip files already contains coding setting
                     if file_content.startswith('# coding='):
                         continue
+
                     # add coding
                     file_content = '# coding=utf-8\n' + file_content
 
@@ -36,13 +39,17 @@ def add_encoding(dir_path):
                     with open(sub_file_path, 'w', encoding='UTF-8') as file:
                         file.write(file_content)
 
+                    print(f'add encoding into file {sub_file_path} successfully.')
+
         if len(sub_dirs) > 0:
             for sub_dir in sub_dirs:
+                if sub_dir in ('__pycache__', '.git'):
+                    continue
                 sub_dir_path = os.path.join(root_path, sub_dir)
                 add_encoding(sub_dir_path)
 
 
 # ===== 主函数 =====
 if __name__ == '__main__':
-    v_dir_path = ''
+    v_dir_path = '..'
     add_encoding(v_dir_path)
