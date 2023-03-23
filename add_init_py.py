1c3d54a956027001cbb1705e7f5deb36726b2d05
xubaoxiang <xubaoxiang@fantac.com.cn>
2023-03-23 09:43:28 +0800

    add __init__.py into every python package dir

---
+++ add_init_py.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+""" 
+description: add __init__.py into every python package dir
+python-version: 3+
+"""
+
+import os
+
+
+def add_init_py(dir_path):
+    """ add init.py file into every python package dir """
+
+    # check directory exists
+    if not os.path.exists(dir_path):
+        print(f'the directory path is not exist: {dir_path}.')
+        return False
+
+    # iterate sub directories
+    for sub_dir in os.listdir(dir_path):
+        sub_dir_path = os.path.join(dir_path, sub_dir)
+
+        # skip non-directory file
+        if not os.path.isdir(sub_dir_path):
+            continue
+
+        # check init.py exists
+        init_py_path = os.path.join(sub_dir_path, '__init__.py')
+        if os.path.exists(init_py_path):
+            print(f'the __init__.py is exist, skip creating: {init_py_path}.')
+            return True
+
+        # create init.py file
+        with open(init_py_path, 'w', encoding='utf-8') as init_py:
+            init_py.write('# coding=utf-8')
+
+        print(f'create __init__.py successfully: {init_py_path}.')
+    return True
+
+
+if __name__ == '__main__':
+    v_dir_path = '.'
+    add_init_py(v_dir_path)
