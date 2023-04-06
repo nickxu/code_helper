e077631309b21f51545b1835f53f6040675c782f
nickxu <xbxmsn@msn.cn>
2023-04-06 11:44:25 +0800

    move outer scripts to common module

--- common/__init__.py
+++ common/__init__.py
@@ -1,2 +1,6 @@
 from .check_dir_file import check_dir_exists, check_file_exists
 from .process_dir_recursively import process_dir_recursively
+from .add_init_py import add_init_py
+from .add_readme import add_readme
+from .change_file_extension import change_file_extension
+from .copy_files import copy_files_under_dir
